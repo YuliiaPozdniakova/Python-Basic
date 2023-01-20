23ccdfb63ba2df112396d53ed14bc602b3f1196e
YuliiaPozdniakova <juliaivanenko055@gmail.com>
2023-01-20 13:50:12 +0200

    update home-work

--- Home_work_11/add_to_file.py
+++ Home_work_11/add_to_file.py
@@ -10,3 +10,12 @@ input_data_1 = input('Введіть рядок 1: ')
 input_data_2 = input('Введіть рядок 2: ')
 input_data_3 = input('Введіть рядок 3: ')
 input_data_4 = input('Введіть рядок 4: ')
+
+item_1 = [input_data_1 + '\n', input_data_2 + '\n']
+item_2 = [input_data_3 + '\n', input_data_4 + '\n']
+
+with open('text_file.txt', 'w') as input_write:
+    input_write.writelines(item_1)
+
+with open('text_file.txt', 'a') as input_write:
+    input_write.writelines(item_2)

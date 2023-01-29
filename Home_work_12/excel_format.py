b23202ad9bfa0a2b80471ee827eb6c94bcdeee57
YuliiaPozdniakova <juliaivanenko055@gmail.com>
2023-01-29 21:22:41 +0200

    fix home-work

--- Home_work_12/excel_format.py
+++ Home_work_12/excel_format.py
@@ -3,77 +3,21 @@
 #
 # К заданию прикреплён пример как должно выглядеть содержания итогового файла.
 import csv
-import openpyxl
-# from openpyxl import Workbook
-from openpyxl.utils import get_column_letter
+from openpyxl import Workbook
 
+wb = Workbook()
+ws = wb.create_sheet(title="Data")
+dest_filename = 'data_xlsx.xlsx'
+age_index = False
 
 with open('data_csv.csv', encoding='utf-8') as read_file:
     file_reader = csv.reader(read_file)
-    data = []
-    for index, item in enumerate(file_reader):
-        if index == 0:
-            name_of_columns = item
-            del name_of_columns[-2]
-        else:
-            data.append(item)
-
-wb = openpyxl.Workbook()
-sheet = wb['Sheet']
-
-# for row_index, value in enumerate(name_of_columns):
-#     cell = sheet.cell(value=row_index + 1, column=row_index + 1)
-#     cell.value = value
-
-
-
-# wb = Workbook()
-#
-# dest_filename = 'empty_book.xlsx'
-#
-# ws3 = wb.create_sheet(title="Data")
-# for row in range(10, 15):
-#     for col in range(5, 10):
-#         ws3.cell(column=col, row=row, value=col)
-#
-# wb.save(filename=dest_filename)
-
-# wb = Workbook()
-#
-# dest_filename = 'empty_book.xlsx'
-#
-# ws4 = wb.create_sheet(title="Customer Data")
-#
-# for col_idx, col_val in enumerate(name_of_columns):
-#     print(type(col_val))
-#     if type(col_val):
-#         ws4.cell(row=1, column=1, value=col_val)
-#     else:
-#         ws4.cell(row=2, column=col_idx * 1, value=col_val)
-#
-# wb.save(filename=dest_filename)
-
-
-for data_list in data:
-    del data_list[-2]
-    # ['Person 6', '523481', 'Becca', '581946311']
-
-    for col_index, col in enumerate(name_of_columns, data_list):
-        for row_index, value in enumerate(col):
-            cell = sheet.cell(row=row_index+1, column=col_index+1)
-            cell.value = value
-
-
-        # print(col_index)
-        # print(col)
-wb.save('data_excel.xlsx')
-
-# for data_list in data:
-#     del data_list[-2]
-#     # ['Person 6', '523481', 'Becca', '581946311']
-#     name_of_columns.append(data_list)
-#
-#     for col_index, col in enumerate(name_of_columns):
-#         for row_index, value in enumerate(col):
-#             cell = sheet.cell(row=row_index+1, column=col_index+1)
-#             cell.value = value
+    for parent_idx, items in enumerate(file_reader):
+        if len(items) != 0:
+            if 'Age' in items:
+                age_index = items.index('Age')
+            if age_index:
+                del items[age_index]
+            for idx, item in enumerate(items):
+                ws.cell(row=idx + 1, column=parent_idx + 1, value=item)
+wb.save(filename=dest_filename)

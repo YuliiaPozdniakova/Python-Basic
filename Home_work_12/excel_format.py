f0037a3b4697f113857d36f6af2408564f5d80a9
YuliiaPozdniakova <juliaivanenko055@gmail.com>
2023-01-29 22:25:35 +0200

    new home-work

--- Home_work_12/excel_format.py
+++ Home_work_12/excel_format.py
@@ -6,18 +6,18 @@ import csv
 from openpyxl import Workbook
 
 wb = Workbook()
-ws = wb.create_sheet(title="Data")
-dest_filename = 'data_xlsx.xlsx'
+sheet = wb['Sheet']
+new_file = 'data_xl.xlsx'
 age_index = False
 
 with open('data_csv.csv', encoding='utf-8') as read_file:
     file_reader = csv.reader(read_file)
     for parent_idx, items in enumerate(file_reader):
-        if len(items) != 0:
-            if 'Age' in items:
-                age_index = items.index('Age')
-            if age_index:
-                del items[age_index]
-            for idx, item in enumerate(items):
-                ws.cell(row=idx + 1, column=parent_idx + 1, value=item)
-wb.save(filename=dest_filename)
+        if 'Age' in items:
+            age_index = items.index('Age')
+        if age_index:
+            del items[age_index]
+        for idx, item in enumerate(items):
+            sheet.cell(row=idx + 1, column=parent_idx + 1, value=item)
+
+wb.save(filename=new_file)
